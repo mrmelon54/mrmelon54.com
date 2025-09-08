8d9dbf5c9c185ebcecfcdf8eb7ce50e03830b0af
MrMelon54 <github@mrmelon54.com>
2025-09-08 21:40:48 +0100

    Fix tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mrmelon54.com',
-    integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  site: 'https://mrmelon54.com',
+  integrations: [mdx(), sitemap(), svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
